c5b83a243664c73da8d1ed27cf4e6444d1dc550e
Karimaboelsoud <karimaboelsoudd@gmail.com>
2025-11-27 16:11:38 +0200

    Update Local-global-variable.cpp

--- Local-global-variable.cpp
+++ Local-global-variable.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int x=100;
+int x=100; //* this is global x, because it is above "main functuion".
 int main ()
 
 {
-int x=5; 
+int x=5; //* this is local x, because it is Inside "main functuion" and above "return 0".
+
 
 cout<<x <<endl;  //! this if you want to print local x which have 5 value.
 
 cout<<::x <<endl; //? this if you want to print global x which have 100 value.
     return 0;
 }
+
